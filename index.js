2c3d22ecf3263af9637baa9678b09c6e307e2d10
Brendan Martin <bmartin@relaynetwork.com>
2020-06-19 13:14:08 -0400

    Intial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const version = require('../package').version;
+
+const env = process.env.ENV_NAME;
+const component = 'custom.looger';
+
+const actionName = (act) => `${component}.${act}`;
+
+exports.log = (act, message, extra) => {
+  console.log({
+    action_name: actionName(act),
+    component,
+    message,
+    version,
+    env,
+    extra
+  });
+};
+
+exports.error = (act, message, err, extra) => {
+  console.error({
+    action_name: actionName(act),
+    component,
+    message,
+    version,
+    env,
+    error: err,
+    extra
+  });
+};
+
+exports.debug = (act, message, extra) => {
+  console.debug({
+    action_name: actionName(act),
+    component,
+    message,
+    version,
+    env,
+    extra
+  });
+};
